db23fa8ca4b695674209cdbfa48c3889ec659431
vitorfail <vitor_andrademanoel@hotmail.com>
2024-01-27 16:43:14 -0300

    melhorando login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import Cli from "../../img/client.png"
 import Pesquisa from '../../components/Pesquisa';
 import Cliente from "../../img/client.png"
+import Cadeado from "../../img/senha.png"
 
 function Login() {
   const [lado, setlado] = useState("35%")
@@ -27,8 +28,28 @@ function Login() {
             <img alt='user' src={Cliente}></img>
             <input placeholder='Usuário'></input>
           </div>
+          <div className='entrada'>
+            <img alt='senha' src={Cadeado}></img>
+            <input type="password" placeholder='Senha'></input>
+          </div>
+          <button>ACESSAR</button>
         </div>
-        <div className='lado-esquerdo'></div>
+        <div className='lado-esquerdo'>
+        <p>CADASTRAR</p>
+          <div className='entrada'>
+            <img alt='user' src={Cliente}></img>
+            <input placeholder='Usuário'></input>
+          </div>
+          <div className='entrada'>
+            <img alt='senha' src={Cadeado}></img>
+            <input type="password" placeholder='Senha'></input>
+          </div>
+          <div className='entrada'>
+            <img alt='senha' src={Cadeado}></img>
+            <input type="password" placeholder='Confirmar senha'></input>
+          </div>
+          <button>REGISTRAR</button>
+        </div>
       </div>
     </div>
   );
