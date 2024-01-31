f02253d66f76b018a3f4f6c44a88374e5335f6ef
vitorfail <vitor_andrademanoel@hotmail.com>
2024-01-30 23:30:27 -0300

    add

--- src/popups/Popup/index.js
+++ src/popups/Popup/index.js
@@ -2,12 +2,13 @@ import { useContext } from "react"
 import "./index.css"
 import { Context } from "../../Provider"
 import Erro from "../../img/erro.png"
+import Ok from "../../img/ok.png"
 export default function Popup(){
     const {popup, setpopup} = useContext(Context)
     return(
         <div id="ok" className={popup?"popup show": "popup"} >
             <div className={popup?"modal show": "modal"}>
-                <img alt="err" src={Erro} ></img>
+                <img alt="err" src={Ok} ></img>
                 <p>Clinte cadsatrado com sucesso!</p>
                 <button onClick={() => setpopup(false)}>Ok</button>
             </div>
